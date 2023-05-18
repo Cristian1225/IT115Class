a11ff04054d3a8143a65a03486bd0cb7a65674bd
Cristian Reyes Chavez <Cristianoreyes234@gmail.com>
2023-05-18 11:46:46 -0700

    updated comments

--- Combobox.py
+++ Combobox.py
@@ -1,15 +1,15 @@
-#
+# we imported tk kinter 
 import tkinter as tk    
 from tkinter import ttk
 
 
-#
+# Here we are stting an event to select an event 
 def on_select(event):
 
     selected_item = event.widget.get()
     print("Selected items:", selected_item)
 
-
+#  Here we created a title for the combobox 
 root = tk.Tk()
 root.title("Combobox Exampple!")
 
