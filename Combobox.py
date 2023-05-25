29310df02d1f923abf1c75b175faaf19d17cfb1a
Cristian Reyes Chavez <Cristianoreyes234@gmail.com>
2023-05-25 10:27:21 -0700

    updated index

--- Combobox.py
+++ Combobox.py
@@ -13,3 +13,17 @@ def on_select(event):
 root = tk.Tk()
 root.title("Combobox Exampple!")
 
+# we are creating an arrays 
+items = [ "Item 1" ,"Item 2","Item 3", "Item4", "Item 5"]
+
+#
+combo_box = ttk.Combobox(root, values=items)
+
+
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+
+combo_box.pack()
+
+
+root.mainloop()
