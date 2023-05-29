cfa52118304aff431781c10c84327837c1c6bd8e
Cristian1225 <cristianoreyes234@gmail.com>
2023-05-29 15:14:51 -0700

    update

--- Combobox.py
+++ Combobox.py
@@ -13,16 +13,16 @@ def on_select(event):
 root = tk.Tk()
 root.title("Combobox Exampple!")
 
-# we are creating an arrays 
+# we are creating arrays 
 items = [ "Item 1" ,"Item 2","Item 3", "Item4", "Item 5"]
 
-#
+# Here we are defining what values equal items
 combo_box = ttk.Combobox(root, values=items)
 
 
 combo_box.bind("<<ComboboxSelected>>", on_select)
 
-
+# Here we are defining the combobox
 combo_box.pack()
 
 
